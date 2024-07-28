6bb8864f0757dd5f6b693d9d3fa29caeb0511713
Mgit125 <cnu.abhib104@gmail.com>
2024-07-28 11:32:28 +0100

    Add files via upload

---
+++ crm_sql.sql
@@ -0,0 +1,22 @@
+-- Date 28/07/2024
+
+--  CRM DATABASE 
+
+select * from accounts;
+
+select * from data_dictionary;
+
+select * from products;
+
+select * from sales_pipeline;
+
+select * from sales_teams;
+
+select * from sales_teams where regional_office = 'West';
+
+select * from sales_teams where sales_agent like 'Vicki%';
+
+
+
+
+
